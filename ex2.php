4bd9d3d138905e89e765126a615057c2e0e12610
lee-rgb1223 <leedavesharvy1223@gmail.com>
2026-02-15 17:51:20 +0800

    Add files via upload

---
+++ ex2.php
@@ -0,0 +1,9 @@
+<?php
+$items = ["Coke", "Royal", "Sprite", "Sting", "Mountain Dew"];
+
+$i = 0;
+while ($i < count($items)) {
+    echo "- " . $items[$i] . "\n";
+    $i++;
+}
+?>
---
+++ ex3.php
@@ -0,0 +1,10 @@
+<?php
+$count = 10;
+
+do {
+    echo $count . "\n";
+    $count--;
+} while ($count >= 1);
+
+echo "Blast off!\n";
+?>
